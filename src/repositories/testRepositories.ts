598a90693358cc7a449331cb0ef9fbc0e0f7cb30
Matheus Balcky <matheuscabal821@gmail.com>
2022-09-16 21:54:53 -0300

    Routes to get tests by discipline and by instructor working

--- src/repositories/testRepositories.ts
+++ src/repositories/testRepositories.ts
@@ -1,4 +1,4 @@
-import prisma from  '../database/database';
+import prisma from '../database/database';
 import * as interfaces from '../interfaces/interfaces';
 
 export async function insertTest(testData: interfaces.testData) {
@@ -10,7 +10,7 @@ export async function testsByDiscipline() {
         select: {
             number: true,
             disciplines: {
-                select:{
+                select: {
                     name: true,
                     teachersDisciplines: {
                         select: {
@@ -19,10 +19,10 @@ export async function testsByDiscipline() {
                                     id: true,
                                     name: true,
                                     pdfUrl: true,
-                                    category: { select: { name: true} },
+                                    category: { select: { name: true } },
                                     teachersDisciplines: {
-                                        select :{
-                                            teacher: { select: { name: true}}
+                                        select: {
+                                            teacher: { select: { name: true } }
                                         }
                                     }
                                 }
@@ -30,41 +30,72 @@ export async function testsByDiscipline() {
                         }
                     }
                 }
-                
+
             }
         }
     });
 
 
-    // return result
-
-
-
-
     return result.map(item => {
         return {
             term: item.number,
-            disciplines: item.disciplines.map( item =>{
+            disciplines: item.disciplines.map(item => {
                 return {
                     name: item.name,
-                    tests: item.teachersDisciplines[0].tests.map(item =>{
+                    tests: item.teachersDisciplines[0].tests.map(item => {
                         return {
                             id: item.id,
                             name: item.name,
                             pdfUrl: item.pdfUrl,
-
                             categoryName: item.category.name,
                             teacherName: item.teachersDisciplines.teacher.name
                         }
                     })
-                } 
-            
+                }
+
             })
-        }})
-
-
+        }
+    });
 }
 
-export async function testsByInstructor () {
+export async function testsByInstructor() {
+    const result = await prisma.teachers.findMany({
+        select: {
+            name: true,
+            teachersDisciplines: {
+                select: {
+                    tests: {
+                        select: {
+                            id: true,
+                            name: true,
+                            pdfUrl: true,
+                            category: { select: { id: true, name: true } },
+                            teachersDisciplines: {
+                                select: { id: true, discipline: true, teacher: { select: { name: true } } }
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    });
 
+    return result.map(item => {
+        return {
+            teacherName: item.name,
+            tests: 
+            item.teachersDisciplines
+            .map(item => item.tests)
+            .reduce( (lst, sub) => lst.concat(sub), [])
+            .map(item =>{
+                return {
+                    id: item.id,
+                    name: item.name,
+                    pdfUrl: item.pdfUrl,
+                    categoryName: item.category.name,
+                    teacherName: item.teachersDisciplines.teacher.name,
+                    disciplineName: item.teachersDisciplines.discipline.name
+                }
+            })
+        }});
 }
